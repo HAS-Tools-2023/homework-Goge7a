f531dab00259c3c405d5d93166e36552c153e5d6
Rob <robertmichucho@yahoo.com>
2023-09-21 10:28:08 -0700

    Homework catch up

---
+++ Homework_Working/week4_numpy_starter.py
@@ -0,0 +1,159 @@
+# Starter code for Homework 4
+
+# %%
+# Import the modules we will use
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# ** MODIFY **
+# Set the file name and path to where you have stored the data
+filename = 'streamflow_week4.txt'
+filepath = os.path.join('../Data/', filename)
+print(os.getcwd())
+print(filepath)
+
+# %%
+# DON'T change this part -- this creates the lists you 
+# should use for the rest of the assignment
+# no need to worry about how this is being done now we will cover
+# this in later sections. 
+
+#Read the data into a pandas dataframe
+data=pd.read_table(filepath, sep = '\t', skiprows=31,
+        names=['agency_cd', 'site_no', 'datetime', 'flow', 'code']
+        )
+
+# Expand the dates to year month day
+data[["year", "month", "day"]] =data["datetime"].str.split("-", expand=True)
+data['year'] = data['year'].astype(int)
+data['month'] = data['month'].astype(int)
+data['day'] = data['day'].astype(int)
+
+flow = data.flow.values.tolist()
+date = data.datetime.values.tolist()
+year = data.year.values.tolist()
+month = data.month.values.tolist()
+day = data.day.values.tolist()
+
+# Make a numpy array of this data
+flow_data = data[['year', 'month','day', 'flow']].to_numpy()
+
+# Getting rid of the pandas dataframe since we wont be using it this week
+del(data)
+
+# %%
+# Starter Code
+# Count the number of values with flow > 600 and month ==7
+flow_count = np.sum((flow_data[:,3] > 600) & (flow_data[:,1]==7))
+
+
+criteria = (flow_data[:, 3] > 600) & (flow_data[:, 1] == 7)
+pick_data = flow_data[criteria, 3]
+flow_mean = np.mean(pick_data)
+
+# Calculate the average flow for these same criteria 
+
+flow_mean = np.mean(flow_data[(flow_data[:,3] > 600) & (flow_data[:,1]==7),3])
+
+print("Flow meets this critera", flow_count, " times")
+print('And has an average value of', flow_mean, "when this is true")
+
+# Make a histogram of data
+# Use the linspace  funciton to create a set  of evenly spaced bins
+mybins = np.linspace(0, 1000, num=15)
+# another example using the max flow to set the upper limit for the bins
+#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15) 
+#Plotting the histogram
+plt.hist(flow_data[:,3], bins = mybins)
+plt.title('Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+# Get the quantiles of flow
+# Two different approaches ---  you should get the same answer
+# just using the flow column
+flow_quants1 = np.nanquantile(flow_data[:,3], q=[0,0.1, 0.5, 0.9])
+print('Method one flow quantiles:', flow_quants1)
+# Or computing on a colum by column basis 
+flow_quants2 = np.nanquantile(flow_data, q=[0,0.1, 0.5, 0.9], axis=0)
+# and then just printing out the values for the flow column
+print('Method two flow quantiles:', flow_quants2[:,3])
+
+
+
+# %%
+#My code
+flow_count = np.sum((flow_data[:,3] >= 0) & (flow_data[:,1]==9))
+
+
+criteria = (flow_data[:, 3] > 600) & (flow_data[:, 1] == 9)
+pick_data = flow_data[criteria, 3]
+flow_mean = np.mean(pick_data)
+
+flow_mean = np.mean(flow_data[(flow_data[:,3] >= 0) & (flow_data[:,1]==7),3])
+
+print("Flow meets this critera", flow_count, " times")
+print('And has an average value of', flow_mean, "when this is true")
+
+# Make a histogram of data
+# Use the linspace  funciton to create a set  of evenly spaced bins
+mybins = np.linspace(0, 1000, num=15)
+# another example using the max flow to set the upper limit for the bins
+#mybins = np.linspace(0, np.max(flow_data[:,3]), num=15) 
+#Plotting the histogram
+plt.hist(flow_data[:,3], bins = mybins)
+plt.title('Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+# Get the quantiles of flow
+# Two different approaches ---  you should get the same answer
+# just using the flow column
+flow_quants1 = np.nanquantile(flow_data[:,3], q=[0,0.1, 0.5, 0.9])
+print('Method one flow quantiles:', flow_quants1)
+# Or computing on a colum by column basis 
+flow_quants2 = np.nanquantile(flow_data, q=[0,0.1, 0.5, 0.9], axis=0)
+# and then just printing out the values for the flow column
+print('Method two flow quantiles:', flow_quants2[:,3])
+# %%
+ilist = []
+for i in range(len(flow)):
+        if month[i] == 9 and day[i] >= 17 and day[i]<= 23:
+                ilist.append(i)
+
+
+# see how many times the criteria was met by checking the length
+# of the index list that was generated
+print(len(ilist))
+
+# Grabbing out the data that met the criteria
+# This  subset of data is just the elements identified
+# in the ilist
+subset = [flow[j] for j in ilist]
+
+
+
+print(np.median(subset))
+
+# %%
+for i in range(len(flow)):
+        if month[i] == 9 and day[i] >= 24 and day[i]<= 30:
+                ilist.append(i)
+
+
+# see how many times the criteria was met by checking the length
+# of the index list that was generated
+#print(len(ilist))
+
+# Grabbing out the data that met the criteria
+# This  subset of data is just the elements identified
+# in the ilist
+subset2 = [flow[i] for i in ilist]
+
+
+
+print(np.median(subset2))
+# %%
